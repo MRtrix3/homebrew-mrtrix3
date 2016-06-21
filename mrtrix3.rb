604d9ec95380738641a1ab1b2425fed284c34f43
Max Pietsch <mail@maxpietsch.com>
2016-06-21 20:03:38 +0200

    added information. added option for nogui. added single threaded build

--- mrtrix3.rb
+++ mrtrix3.rb
@@ -1,8 +1,9 @@
-class Qt5Requirement < Requirement
+  class Qt5Requirement < Requirement
   fatal true
 
   satisfy :build_env => false do
     @qmake = which('qmake')
+    # @qmake = HOMEBREW_PREFIX"/opt/qt5/bin/qmake"
     @qmake
   end
   
@@ -12,15 +13,28 @@ class Qt5Requirement < Requirement
   
   def message
     message = <<-EOS.undent
-      Homebrew was unable to find an installation of Qt 5. You can install it with:
+      Homebrew was unable to find an installation of Qt 5. You can install it manually or with
       brew install qt5
+
+      and make sure that it is found with
+      export PATH=`brew --prefix`/opt/qt5/bin:$PATH
+      or
       brew link --force qt5
     EOS
   end
 end
 
+ENV['LIBLEPT_HEADERSDIR'] = HOMEBREW_PREFIX/"include"
+
 class Mrtrix3 < Formula
-  desc "MRtrix provides a set of tools to perform diffusion-weighted MRI white matter tractography in the presence of crossing fibres, using Constrained Spherical Deconvolution (Tournier et al.. 2004; Tournier et al. 2007), and a probabilisitic streamlines algorithm (Tournier et al., 2012). These applications have been written from scratch in C++, using the functionality provided by Eigen, and Qt. The software is currently capable of handling DICOM, NIfTI and AnalyseAVW image formats, amongst others. The source code is distributed under the Mozilla Public License."
+  desc "MRtrix provides a set of tools to perform diffusion-weighted MRI white matter tractography in the presence of crossing fibres, 
+    using Constrained Spherical Deconvolution (Tournier et al.. 2004; Tournier et al. 2007), and a probabilisitic streamlines algorithm 
+    (Tournier et al., 2012). These applications have been written from scratch in C++, using the functionality provided by Eigen, and Qt. 
+    The software is currently capable of handling DICOM, NIfTI and AnalyseAVW image formats, amongst others. The source code is distributed 
+    under the Mozilla Public License.
+
+    For more information on how to install and use MRtrix go to http://mrtrix.readthedocs.io
+    "
   homepage "mrtrix.org"
   url "https://github.com/MRtrix3/mrtrix3/archive/0.3.15.tar.gz"
   version "0.3.15"
@@ -28,8 +42,10 @@ class Mrtrix3 < Formula
 
   depends_on "eigen" => :build
   depends_on "pkg-config" => :build
-  # depends_on "qt5"
+  # depends_on "qt5" # not used as users might want to use an existing qt or install mrtrix without a GUI
   depends_on Qt5Requirement => :recommended
+
+  option "build_single_thread", "This is useful if your computer has many cores but not enough RAM to build MRtrix using multiple threads."
 
   def execute (cmd)
     # verbose alternative to: system cmd
@@ -50,8 +66,18 @@ class Mrtrix3 < Formula
   end
 
   def install
-    execute ("./configure")
-    execute ("./build release")
+
+    if build.include? "build_single_thread"
+      ENV["NUMBER_OF_PROCESSORS"] = "1"
+    end
+
+    conf = [ "./configure" ]
+    if build.include? "without-qt5"
+      conf.push("-nogui")
+    end
+    execute (conf.join(" "))
+
+    execute ("./build")
 
     bin.install Dir["release/bin/*"]
     bin.install Dir["scripts/*"]
