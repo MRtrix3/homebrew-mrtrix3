4126c0eb5be6b0c9cb9eca11f9d7570e9efddc9d
Max Pietsch <mail@maxpietsch.com>
2016-08-03 18:11:43 +0100

    update_formula: delete temporary directory

--- update_formula.py
+++ update_formula.py
@@ -26,39 +26,40 @@ def parse_remote():
     return describe
 
 if __name__ == '__main__':
-    import subprocess, tempfile
+    import subprocess, tempfile, shutil
 
     tempdir = tempfile.mkdtemp()
-    print (tempdir)
-    os.chdir(tempdir)
-    subprocess.call("git clone https://github.com/MRtrix3/homebrew-mrtrix3.git", shell = True)
-    os.chdir(tempdir+"/homebrew-mrtrix3")
+    try:
+        print (tempdir)
+        os.chdir(tempdir)
+        subprocess.call("git clone https://github.com/MRtrix3/homebrew-mrtrix3.git", shell = True)
+        os.chdir(tempdir+"/homebrew-mrtrix3")
 
-    with open ('mrtrix3.rb', 'r') as fin:
-        formula = fin.readlines()
-    current_line = None
-    for iline, line in enumerate(formula):
-        if line.lstrip().startswith('version'):
-            current_line = line
-            break
+        with open ('mrtrix3.rb', 'r') as fin:
+            formula = fin.readlines()
+        current_line = None
+        for iline, line in enumerate(formula):
+            if line.lstrip().startswith('version'):
+                current_line = line
+                break
 
-    if current_line is None:
-        raise Exception ("version parsing failed")
-    current = current_line.split()[1].replace("'", "")
-    print ("formula: " + current)
+        if current_line is None:
+            raise Exception ("version parsing failed")
+        current = current_line.split()[1].replace("'", "")
+        print ("formula: " + current)
 
-    remote = parse_remote()
-    print ("remote: " + remote)
+        remote = parse_remote()
+        print ("remote: " + remote)
 
-    if remote != current:
-        formula[iline] = current_line.replace(current, remote)
-        with open ('mrtrix3.rb', 'w') as fout:
-            fout.writelines(formula)
-        subprocess.call('git add mrtrix3.rb', shell = True)
-        subprocess.call('git commit -m "automatic formula update to ' + remote + '"', shell = True)
-        subprocess.call('git push', shell = True)
-
-
+        if remote != current:
+            formula[iline] = current_line.replace(current, remote)
+            with open ('mrtrix3.rb', 'w') as fout:
+                fout.writelines(formula)
+            subprocess.call('git add mrtrix3.rb', shell = True)
+            subprocess.call('git commit -m "automatic formula update to ' + remote + '"', shell = True)
+            subprocess.call('git push', shell = True)
+    finally:
+        shutil.rmtree(tempdir, ignore_errors=False, onerror=None)
 
 
 
