bd9e84d5dcded8d9e8a856147e5754d0b9916269
maxpietsch <maxpietsch@users.noreply.github.com>
2017-02-23 20:01:54 +0000

    rev 1

--- mrtrix3.rb
+++ mrtrix3.rb
@@ -37,7 +37,7 @@ class Mrtrix3 < Formula
   url "https://github.com/MRtrix3/mrtrix3.git"
 
   version  '0.3.15-491-g4901e0f'
-  revision 0
+  revision 1
 
   head "https://github.com/MRtrix3/mrtrix3.git"
 
