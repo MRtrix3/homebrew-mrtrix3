62db4ed40802fed0582a48875f41e6fb18632ab0
Max Pietsch <mail@maxpietsch.com>
2017-05-23 00:00:06 +0100

    automatic formula update to 3.0_RC1-60-gfebedf5b

--- mrtrix3.rb
+++ mrtrix3.rb
@@ -36,9 +36,8 @@ class Mrtrix3 < Formula
 
   url "https://github.com/MRtrix3/mrtrix3.git"
 
-  version  '3.0_RC1-47-g3b789609'
+  version  '3.0_RC1-60-gfebedf5b'
 revision 0  
-  
   option "stable", "Install latest tagged stable version. Default is last commit on master branch."
   option "test", "Run tests after installation."
   option "assert", "Build with assert statements (executables are slower)."
