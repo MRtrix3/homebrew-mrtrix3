42a9f22b7630279ffe81cdcd573c56d9e62408e8
Max Pietsch <mail@maxpietsch.com>
2017-05-27 00:00:06 +0100

    automatic formula update to 3.0_RC1-67-ga15220de

--- mrtrix3.rb
+++ mrtrix3.rb
@@ -36,9 +36,8 @@ class Mrtrix3 < Formula
 
   url "https://github.com/MRtrix3/mrtrix3.git"
 
-  version  '3.0_RC1-65-gb19f24ab'
-revision 0  option "stable", "Install latest tagged stable version. Default is last commit on master branch."
-  option "test", "Run tests after installation."
+  version  '3.0_RC1-67-ga15220de'
+revision 0  option "test", "Run tests after installation."
   option "assert", "Build with assert statements (executables are slower)."
   option "debug", "Build with debug statements (executables are slower)."
   option "mrconvert", "Build mrconvert, no other binaries unless stated"
