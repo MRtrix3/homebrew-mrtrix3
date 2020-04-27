fda2830983e659b3f55b9a88529c99a94face75b
Max Pietsch <mail@maxpietsch.com>
2020-04-27 02:00:06 +0100

    automatic formula update to 3.0.0-0-g047e21f3

--- mrtrix3.rb
+++ mrtrix3.rb
@@ -36,7 +36,7 @@ class Mrtrix3 < Formula
 
   url "https://github.com/MRtrix3/mrtrix3.git"
 
-  version  '3.0_RC4-90-gcc3600ed'
+  version  '3.0.0-0-g047e21f3'
   revision 0
   # devel do
   #   url 'https://github.com/MRtrix3/mrtrix3.git', :branch => 'master', :revision => 'bogus474279845b7e79fc2b5ffad'
